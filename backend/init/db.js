aace00077f51249d2972f9b066e856ebf7ac786b
Krishnavardhan2305 <captainkv.2511@gmail.com>
2025-03-26 02:32:21 +0530

    connected to cloud

--- backend/init/db.js
+++ backend/init/db.js
@@ -1,27 +1,28 @@
 import pg from "pg";
 import dotenv from "dotenv";
 const { Client } = pg;
-dotenv.config({});
+dotenv.config();
+
 const db = new Client({
-    user: process.env.PG_USERNAME, 
-    password:process.env.PG_PWD,
-    host: "localhost",
+    user: process.env.PG_USERNAME,
+    password: process.env.PG_PWD,
+    host: process.env.PG_HOST,  
     database: process.env.PG_DB,
-    port: process.env.PG_PORT,
-  });
-  
-
-try {   
-  await db.connect();
-  console.log("Connected to PostgreSQL database!");
-} catch (error) {
-  console.error("Database connection error:", error);
-  process.exit(1);
-}
-db.on("error", (err) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(1);
+    port: process.env.PG_PORT,  
 });
-export const query = (text, params) => db.query(text, params);
 
+try {
+    await db.connect();
+    console.log("Connected to PostgreSQL database!");
+} catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1);
+}
+
+db.on("error", (err) => {
+    console.error("Unexpected error on idle client", err);
+    process.exit(1);
+});
+
+export const query = (text, params) => db.query(text, params);
 export default db;
