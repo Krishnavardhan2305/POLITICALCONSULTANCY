e8fcc13bff9a5799b1af9ed907684d1817ecf97c
Krishnavardhan2305 <captainkv.2511@gmail.com>
2025-03-26 10:57:02 +0530

    pg to supabase

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,6 +1,6 @@
 import * as clientService from "../services/adminServices.js";
 import * as reviewServices from "../services/reviewServices.js";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
