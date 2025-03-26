b5703cdf441d59acab7229c0f4feeed74441e34a
Krishnavardhan2305 <captainkv.2511@gmail.com>
2025-03-26 10:45:31 +0530

    pg to supabase

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
 import adminroutes from './routes/adminroutes.js';
-
+import path from "path"
 const app = express();
 dotenv.config();
+
+const _dirname=path.resolve();
 
 // Allow JSON payloads in request bodies
 app.use(express.json());
@@ -12,12 +14,12 @@ app.use(express.json());
 // Define allowed origins for CORS
 const allowedOrigins = [
     "http://localhost:5173",  // Local development frontend
+    "http://localhost:8080",  // Local development frontend
     "https://politicalconsultancy-pegg.vercel.app"  // Production frontend on Vercel
 ];
 
 const corsOptions = {
     origin: function (origin, callback) {
-        // Allow requests if the origin is in the allowedOrigins array or if origin is undefined (like in Postman)
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
         } else {
@@ -28,11 +30,15 @@ const corsOptions = {
     credentials: true,  // Allow cookies and other credentials
 };
 
-// Apply CORS middleware globally
 app.use(cors(corsOptions));
 
 // API routes
 app.use('/api/v1/adminroutes', adminroutes);
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+})
 
 // Start server on specified PORT from .env, fallback to 8080
 const PORT = process.env.PORT || 8080;
